49fde9e19deb193ce37c4bab89c6466262a57b0c
Aquite <pavel.batalov@hotmail.com>
2024-01-14 12:59:54 -0800

    list of professions and skills

--- src/routes/character-sheet.jsx
+++ src/routes/character-sheet.jsx
@@ -2,6 +2,52 @@ const CharacterSheet = () => {
   return (
     <>
       <h1>Character Sheet</h1>
+      <h2>Career</h2>
+      <ul>
+        <li>Marine</li>
+        <li>Colonial Marhal</li>
+        <li>Company Agent</li>
+        <li>Medic</li>
+        <li>Officer</li>
+        <li>Pilot</li>
+        <li>Roughneck</li>
+        <li>Scientist</li>
+      </ul>
+      <h2>Attributes and Skills</h2>
+      <ul>
+        <li>
+          Strength
+          <ul>
+            <li>Heavy Machinery</li>
+            <li>Stamina</li>
+            <li>Close Combat</li>
+          </ul>
+        </li>
+        <li>
+          Agility
+          <ul>
+            <li>Mobility</li>
+            <li>Piloting</li>
+            <li>Ranged Combat</li>
+          </ul>
+        </li>
+        <li>
+          Wits
+          <ul>
+            <li>Observation</li>
+            <li>Comtech</li>
+            <li>Survival</li>
+          </ul>
+        </li>
+        <li>
+          Empathy
+          <ul>
+            <li>Manipulation</li>
+            <li>Medical Aid</li>
+            <li>Command</li>
+          </ul>
+        </li>
+      </ul>
     </>
   );
 };
